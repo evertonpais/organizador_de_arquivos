89a394e9d073f58de6ec73e0fe08cbf0c781bf7b
evertonpais <98322981+evertonpais@users.noreply.github.com>
2023-10-12 18:23:28 -0300

    Add files via upload

--- projeto1.py
+++ projeto1.py
@@ -1,39 +1,43 @@
-import os
 from tkinter import *
 from tkinter.filedialog import askdirectory
+import os
+import shutil
+
+def organizar_arquivos():
+    caminho = askdirectory(title='Selecione uma pasta')
+    if not caminho:
+        return
+
+    locais = {
+        'imagens': ['.png', '.jpeg','JPEG'],
+        'planilhas': ['.xlsx'],
+        'pdfs': ['.pdf'],
+        'documentos do word': ['.docx'],
+        'arquivos jw': ['.jwpub'],
+        'videos': ['.mp4']
+    }
+
+    lista_arquivos = os.listdir(caminho)
+    for arquivo in lista_arquivos:
+        nome, extensao = os.path.splitext(arquivo)
+        extensao = extensao.lower()
+        for pasta, extensoes in locais.items():
+            if extensao in extensoes:
+                pasta_destino = os.path.join(caminho, pasta)
+                os.makedirs(pasta_destino, exist_ok=True)
+                origem_arquivo = os.path.join(caminho, arquivo)
+                destino_arquivo = os.path.join(pasta_destino, arquivo)
+                if not os.path.exists(destino_arquivo):
+                    shutil.move(origem_arquivo, destino_arquivo)
 
 janela = Tk()
-janela.title('organize seus arquivos')
+janela.title('Organize seus arquivos')
 janela.geometry('400x400')
 
-texto_orientacao = Label(janela, text = 'clique no botão para selecionar a pasta que deseja organizar')
-texto_orientacao.grid(column=0,row=0)
-botao_selecionar = Button(janela, text= 'selecione a pasta',command=askdirectory)  
-botao_selecionar.grid(column=0,row=1)
-      
+texto_orientacao = Label(janela, text='Clique no botão para selecionar a pasta que deseja organizar')
+texto_orientacao.grid(column=0, row=0)
 
+botao_selecionar = Button(janela, text='Selecione a pasta', command=organizar_arquivos)
+botao_selecionar.grid(column=0, row=1)
 
 janela.mainloop()
-
-
-caminho = askdirectory(title= 'selecione uma pasta')
-
-lista_arquivos = os.listdir(caminho)
-
-
-locais = {
-    'imagens': ['.png','.jpg'],
-    'planilhas': ['.xlsx'],
-    'pdfs': ['.pdf','.PDF'],
-    'documentos do word' : ['.docx'],
-    'arquivos jw' : ['.jwpub']
-}
-for arquivo in lista_arquivos:
-    nome, extensao = os.path.splitext(f'{caminho}/{arquivo}')
-    for pasta in locais:
-        if extensao in locais[pasta]:
-            if not os.path.exists(f'{caminho}/{pasta}'):
-                os.mkdir(f'{caminho}/{pasta}')
-            os.rename(f'{caminho}/{arquivo}', f'{caminho}/{pasta}/{arquivo}')
-
-   
